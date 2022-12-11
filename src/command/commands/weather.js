9d3f7b15bf18e6ceadba8b12e8ae42a1cfbdc5e0
Joe Woods <code@joewoods.dev>
2022-12-11 17:33:56 -0500

    Add weather command
    Also straight from Quirk.

---
+++ src/command/commands/weather.js
@@ -0,0 +1,38 @@
+import { Command } from "../Command.js";
+
+const getWeather = function resolveLocationToWeatherString(location) {
+  return new Promise((resolve) => {
+    fetch(`https://wttr.in/${location}?format=j1`)
+      .then((res) => res.json())
+      .then((res) =>
+        resolve(
+          `${location}: ${res.current_condition[0].weatherDesc[0].value}, ${res.current_condition[0].temp_C}°C (${res.current_condition[0].temp_F}°F)`
+        )
+      );
+  });
+};
+
+class WeatherCommand extends Command {
+  constructor() {
+    super("weather");
+  }
+
+  async input(input) {
+    let location = input.textWithoutCommandName();
+    let responseText = "";
+
+    if (!location) {
+      location = "Philadelphia";
+
+      responseText += "I don't know where you are!\n";
+    }
+
+    return getWeather(location).then((res) =>
+      this.responseFromText(responseText + res)
+    );
+  }
+}
+
+const weather = new WeatherCommand();
+
+export { weather };
