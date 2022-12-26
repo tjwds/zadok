8dbc0290b8a6a6e7d05eede8b32038dbb2edad01
Joe Woods <code@joewoods.dev>
2022-12-26 17:34:13 -0500

    todo: warn on command name mix-up

--- src/command/commands/todo.js
+++ src/command/commands/todo.js
@@ -1,4 +1,5 @@
 import { Command } from "../Command.js";
+import { Response, WARNING } from "../../response/Response.js";
 
 // I know, I know, I called the other TodoCommand that and this todo.  Sorry.
 
@@ -8,6 +9,13 @@ class ThingsTodoCommand extends Command {
   }
 
   async input(input) {
+    if (input.words.length > 1) {
+      const response = new Response(
+        "I don't know that command.  You probably wrote `todo` when you meant `task` or something."
+      );
+      response.type = WARNING;
+      return response;
+    }
     input.channel.parseInput("goal", {
       event: input.extra?.event,
     });
